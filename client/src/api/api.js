5e17c57d489cdc5ccc9caed761dba36531123130
Sahil <sahilwadhawan16@gmail.com>
2025-07-01 04:09:54 +0530

    Updated API base URL

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',  // Change to your deployed backend later
+  baseURL: `${BASE_URL}`,
 });
 
+// API functions
 export const fetchProducts = () => API.get('/products');
 export const addProduct = (product) => API.post('/products/add', product);
 export const reduceStock = (id, quantity) => API.put('/products/reduce', { id, quantity });
 export const deleteProduct = (id) => API.delete(`/products/${id}`);
-export const updateProduct = (id, updatedData) =>
-  axios.put(`http://localhost:5000/api/products/${id}`, updatedData);
+export const updateProduct = (id, updatedData) => API.put(`/products/${id}`, updatedData);
