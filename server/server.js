dc024380f8bcaa89db1d4482cc3b30f826a5e88e
Sahil <sahilwadhawan16@gmail.com>
2025-07-01 05:25:02 +0530

    fix: server code

--- server/server.js
+++ server/server.js
@@ -7,13 +7,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/payment", require("./routes/paymentRoutes"));
+app.use("/api/chat", require("./routes/chatRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-app.use("/api/payment", require("./routes/paymentRoutes"));
-const chatRoutes = require("./routes/chatRoutes");
-app.use("/api/chat", chatRoutes);
